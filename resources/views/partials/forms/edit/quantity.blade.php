b4586f5010ccd672d696a5bf74d403dc9dc1e226
Roman Yahin <roman.nadym@yandex.ru>
2024-12-13 13:57:38 +0300

    Обязательные поля расположение

--- resources/views/partials/forms/edit/quantity.blade.php
+++ resources/views/partials/forms/edit/quantity.blade.php
@@ -4,7 +4,7 @@
     <label for="qty" class="col-md-3 control-label">{{ trans('general.quantity') }}</label>
     <div class="col-md-7">
        <div class="col-md-3" style="padding-left:0px">
-           <input class="form-control" maxlength="5" type="text" name="qty" aria-label="qty" id="qty" value="{{ old('qty', $item->qty) }}" {!!  (Helper::checkIfRequired($item, 'qty')) ? ' required ' : '' !!}/>
+           <input class="form-control" maxlength="5" type="text" name="qty" aria-label="qty" id="qty" value="{{ old('qty', $item->qty) ? old('qty', $item->qty) : '1' }}" {!!  (Helper::checkIfRequired($item, 'qty')) ? ' required ' : '' !!}/>
        </div>
         <div class="col-md-12" style="padding-left:0px">
        {!! $errors->first('qty', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
